59be336efa05ddc3f4003bd04507703094e0a51d
jmd <jmd2028@gmail.com>
2014-04-22 02:14:32 -0700

    plugins/solstice: Make sure effect was actually applied before modifying combat info.

--- plugins/solstice/hooks/h_Effects.cpp
+++ plugins/solstice/hooks/h_Effects.cpp
@@ -12,7 +12,7 @@ int32_t Hook_OnApplyDamageImmunityDecrease(
     int32_t res = CNWSEffectListHandler__OnApplyDamageImmunityDecrease_orig(elh, obj, eff, a);
     int32_t idx = ns_BitScanFFS(eff->eff_integers[0]);
 
-    if ( idx >= 0 && idx < DAMAGE_INDEX_NUM && res == 0 && obj->obj_type == 5) {
+    if ( res == 0 && idx >= 0 && idx < DAMAGE_INDEX_NUM && res == 0 && obj->obj_type == 5) {
         auto cre = Local_GetCreature(obj->obj_id);
         if (cre) {
             cre->info.defense.immunity[idx] -= eff->eff_integers[1];
@@ -32,7 +32,7 @@ int32_t Hook_OnApplyDamageImmunityIncrease(
 
     int32_t idx = ns_BitScanFFS(eff->eff_integers[0]);
 
-    if ( idx >= 0 && idx < DAMAGE_INDEX_NUM && res == 0 && obj->obj_type == 5) {
+    if ( res == 0 && idx >= 0 && idx < DAMAGE_INDEX_NUM && res == 0 && obj->obj_type == 5) {
         auto cre = Local_GetCreature(obj->obj_id);
         if (cre) {
             cre->info.defense.immunity[idx] += eff->eff_integers[1];
