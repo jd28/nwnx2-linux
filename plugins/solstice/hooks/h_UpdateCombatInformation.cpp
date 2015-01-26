1f1887599fbec71a04b3a2c25125e1c784a25811
jmd <jmd2028@gmail.com>
2015-01-26 04:24:13 -0800

    plugins/solstice: Only call UpdateCombatInformation hook if combat engine is active.

--- plugins/solstice/hooks/h_UpdateCombatInformation.cpp
+++ plugins/solstice/hooks/h_UpdateCombatInformation.cpp
@@ -8,16 +8,18 @@ extern lua_State *L;
 void Hook_UpdateCombatInformation(CNWSCreatureStats *stats) {
     CNWSCreatureStats__UpdateCombatInformation_orig(stats);
 
-    ++solstice.updates;
-    auto start = ClockGetTime();
-    if(!nl_pushfunction(L, "NWNXSolstice_UpdateCombatInfo"))
-        return;
+    if (solstice.combat_engine_active) {
+        ++solstice.updates;
+        auto start = ClockGetTime();
+        if(!nl_pushfunction(L, "NWNXSolstice_UpdateCombatInfo"))
+            return;
 
-    lua_pushinteger(L, stats->cs_original->obj.obj_id);
+        lua_pushinteger(L, stats->cs_original->obj.obj_id);
 
-    if (lua_pcall(L, 1, 0, 0) != 0){
-        solstice.Log(0, "SOLSTICE: NWNXSolstice_UpdateCombatInfo : %s\n", lua_tostring(L, -1));
-        lua_pop(L, 1);
+        if (lua_pcall(L, 1, 0, 0) != 0){
+            solstice.Log(0, "SOLSTICE: NWNXSolstice_UpdateCombatInfo : %s\n", lua_tostring(L, -1));
+            lua_pop(L, 1);
+        }
+        solstice.update_time += ClockGetTime() - start;
     }
-    solstice.update_time += ClockGetTime() - start;
 }
