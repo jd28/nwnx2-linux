a769c1194147402df238d365d74678c6bea9f52d
jmd <jmd2028@gmail.com>
2015-09-21 00:11:18 -0700

    plugins/solstice: Add handlers for running scripts/cmds
    This may be useful occassionally.

--- plugins/solstice/handle_request.cpp
+++ plugins/solstice/handle_request.cpp
@@ -24,6 +24,39 @@ char* HandleRequest(CGameObject *ob, const char *request, char *value)
             lua_pop(L, 1);
         }
     }
+    else if (M(request, "RUNSCRIPT")) {
+        if (!nl_pushfunction(L, "_SOL_RUN_SCRIPT"))
+            return 0;
+
+        lua_pushstring(L, value);
+        lua_pushinteger(L, ob->id);
+
+        if (lua_pcall(L, 2, 2, 0) != 0) {
+            solstice.Log(0, "ERROR: %s on %x %s\n", value, ob->id, lua_tostring(L, -1));
+            lua_pop(L, 1);
+            return 0;
+        }
+
+        int res = lua_tointeger(L, -1);
+        int ret = -1;
+        if (res != 0) {
+            ret = lua_tointeger(L, -2);
+        }
+        lua_pop(L, 2);
+        snprintf(value, strlen(value),"%d", ret);
+    }
+    else if (M(request, "RUNCOMMAND")) {
+        if (!nl_pushfunction(L, "_RUN_COMMAND"))
+            return NULL;
+
+        lua_pushinteger(L, atoi(value));
+
+        if (lua_pcall(L, 1, 0, 0) != 0) {
+            solstice.Log(0, "Error: _RUN_COMMAND : %s\n", lua_tostring(L, -1));
+            lua_pop(L, 1);
+            return NULL;
+        }
+    }
 
     return NULL;
 #undef M
