e2edb8cc208d553c309e12c2e3bf910036161361
jmd <jmd2028@gmail.com>
2016-02-05 05:17:52 -0800

    api: Fill in CNWLevelStats

--- api/CNWLevelStats.h
+++ api/CNWLevelStats.h
@@ -1,6 +1,7 @@
 #ifndef _CNWLEVELSTATS_H_
 #define _CNWLEVELSTATS_H_
 #include "nwndef.h"
+#include "CExoArrayList.h"
 
 class CNWLevelStats
 {
@@ -12,5 +13,18 @@ public:
     ~CNWLevelStats();
     CNWLevelStats();
 
+    CExoArrayList<unsigned long> SpellsKnown[10];
+    CExoArrayList<unsigned long> SpellsRemoved[10];
+    CExoArrayList<unsigned short> FeatList;
+    uint8_t *Skills;
+    /* 0100 */ uint16_t Skillpoints;
+    uint8_t Ability;
+    uint8_t HP;
+    /* 0104 */ uint8_t Class;
+    /* 0105 */ uint8_t rsvd1[3];
+    /* 0108 */ uint8_t EpicLevel;
+    /* 0109 */ uint8_t rsvd2[3];
 };
+
+static_assert(sizeof(CNWLevelStats) == 0x10C, "CNWLevelStats has an incorrect size");
 #endif
