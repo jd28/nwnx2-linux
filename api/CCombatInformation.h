f108d54af1eb83e310b9e80bc42c1035834be941
jmd <jmd2028@gmail.com>
2016-02-05 09:57:26 -0800

    api: Fill in CCombatInformation

--- api/CCombatInformation.h
+++ api/CCombatInformation.h
@@ -1,6 +1,8 @@
 #ifndef _CCOMBATINFORMATION_H_
 #define _CCOMBATINFORMATION_H_
 #include "nwndef.h"
+#include "CExoArrayList.h"
+#include "CExoString.h"
 
 class CCombatInformation
 {
@@ -13,5 +15,49 @@ public:
     int operator==(CCombatInformation &);
     int operator!=(CCombatInformation &);
 
+    uint8_t NumAttacks;
+    uint8_t OnhandAttackMod;
+    uint8_t OnhandDamageMod;
+    uint8_t OffhandAttackMod;
+    uint8_t OffhandDamageMod;
+    uint8_t Spell_resistance;
+    uint8_t ArcaneSpellFailure;
+    uint8_t ArmorCheckPenalty;
+    uint8_t UnarmedDamageDice;
+    uint8_t UnarmedDamageDie;
+    uint8_t field_0A;  // Creature attack dice...
+    uint8_t field_0B;
+    uint8_t field_0C;
+    uint8_t field_0D;
+    uint8_t field_0E;
+    uint8_t field_0F;
+    uint8_t field_10;
+    uint8_t field_11;
+    uint8_t field_12;
+    uint8_t OnhandCritRange;
+    uint8_t OnhandCritMult;
+    uint8_t field_15;
+    uint8_t field_16;
+    uint8_t field_17;
+    uint8_t IsOffhandWeaponEquipped;
+    uint8_t field_19;
+    uint8_t field_1A;
+    uint8_t field_1B;
+    uint8_t OffhandCritRange;
+    uint8_t OffhandCritMult;
+    uint8_t field_1E;
+    uint8_t field_1F;
+    CExoArrayList<CCombatInformationNode*> AttackMods;
+    CExoArrayList<CCombatInformationNode*> DamageMods;
+    uint32_t RighthandObject;
+    uint32_t LefthandObject;
+    CExoString RightString;
+    CExoString LeftString;
+    uint8_t DamageDice;
+    uint8_t DamageDie;
+    uint8_t field_52;
+    uint8_t field_53;
 };
+
+static_assert(sizeof(CCombatInformation) == 0x54, "CCombatInformation has an incorrect size.");
 #endif
